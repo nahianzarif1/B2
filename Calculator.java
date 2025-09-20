3c84ff1d5689889aebca3f66c2bb681ae922560d
nahianzarif1 <nahianzarif.100@gmail.com>
2025-09-20 23:12:50 +0600

    modified add method in main branch

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        // Changed in main
+        return a + b + 1; // intentionally different
+    }
+    
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Add: " + calc.add(10, 5));
+        System.out.println("Subtract: " + calc.subtract(10, 5));
+        System.out.println("Multiply: " + calc.multiply(10, 5));
+        System.out.println("Divide: " + calc.divide(10, 5));
+    }
+}
